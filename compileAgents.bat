727a2c9d4ad90ac3c3950d3d4c86e32f8cb427e1
RAZIMBAUD Guillaume <grazimbaud@gmail.com>
2014-01-16 11:33:51 +0100

    upadte *.bat

--- compileAgents.bat
+++ compileAgents.bat
@@ -1,2 +1,2 @@
 mkdir classes
-javac -cp lib\jade.jar;lib\jmathplot.jar -d classes src\asimilProject\eval1\*.java src\asimilProject\eval2\*.java src\asimilProject\intfce\*.java src\asimilProject\pedagogique\*.java src\asimilProject\traceur\*.java src\asimilProject\utils\*.java
+javac -encoding UTF-8 -cp lib\jade.jar;lib\jmathplot.jar -d classes src\asimilProject\eval1\*.java src\asimilProject\eval2\*.java src\asimilProject\intfce\*.java src\asimilProject\pedagogique\*.java src\asimilProject\traceur\*.java src\asimilProject\utils\*.java
