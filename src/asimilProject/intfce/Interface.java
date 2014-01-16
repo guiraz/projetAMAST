6dd75858a156ee128832b03eaaa7666020002eb3
RAZIMBAUD Guillaume <grazimbaud@gmail.com>
2014-01-16 11:33:22 +0100

    Update Interface.java
    remove time accelerator

--- src/asimilProject/intfce/Interface.java
+++ src/asimilProject/intfce/Interface.java
@@ -92,7 +92,7 @@ public class Interface extends Agent {
 	}
 
 	public void waitAndSend(String mess, int timer) {
-		doWait(timer/10);
+		doWait(timer);
 		String[] receiver = new String[] {"eval1","eval2", "traceur"};
 		addBehaviour(new OneMessageBehaviour(this, receiver, ACLMessage.CFP, mess));
 	}
